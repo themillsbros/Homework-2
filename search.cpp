102ab67473a1b7c6986a5266fc7583476d80cfd2
BRODY MILLS <bdm127@txstate.edu>
2020-09-11 03:00:15 +0000

    Question 1 edits

--- search.cpp
+++ search.cpp
@@ -1,13 +1,37 @@
 /****
 * PSEUDOCODE:
-*
+* Linear: Use for loop to cycle through array until match found.
+* Binary: while loop to cut array in half using equilvancy statements
+* until match found 
 */
 int LinearSearch(int numbers[], int length, int n) {
     // TODO implement this function
-   return 1;
+    for( int i = 0; i < length; i++) {
+      if ( numbers[i] ==  n) {
+        return i;
+      }  
+    }
+   return -1;
 }
 
 int BinarySearch(int numbers[], int length, int n) {
     // TODO implement this function
-     return 1;
-}
+      int first = 0;
+      int last = length - 1;
+      int middle;
+      int position = -1;
+      bool found = false;
+
+      while (!found && first <= last){
+        middle = (first + last)/2;
+        if (numbers[middle] == n){
+          found = true;
+          position = middle;
+        }
+        else if (numbers[middle] > n){
+          last = middle - 1;
+        }
+        else{
+          first = middle + 1;
+        }
+      }
